c4e68ca44f90c106da08bc81a82e1b5a1622d8a1
Marcos Paulo <lira.santos@academico.ifpb.edu.br>
2026-09-23 11:29:51 -0300

    ajeistando a correção

--- cadastro/views.py
+++ cadastro/views.py
@@ -455,10 +455,18 @@ Prefeitura Municipal de Picuí
                 return redirect("cadastro_sucesso")
 
             except Exception:
+                import traceback
+
+                print("\n" + "=" * 70)
+                print("ERRO AO SALVAR CADASTRO:")
+                print(str(e))
+                print("=" * 70)
+                traceback.print_exc()
+                print("=" * 70 + "\n")
 
                 messages.error(
                     request,
-                    ("Ocorreu um erro ao salvar " "o cadastro. Tente novamente."),
+                    (f"Erro ao salvar o cadastro: {e}"),
                 )
 
         # ==========================================
